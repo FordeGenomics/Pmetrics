6825ebec5cc8af2c05eafc58528385e5451a585d
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-09-20 15:18:11 -0500

    one execution per subject

--- R/oneATT.R
+++ R/oneATT.R
@@ -1,3 +1,13 @@
-oneATT<-function(file_path, data_path, engine, ...){
-    
+oneATT<-function(model_path, data_object, engine, ...){
+    IDs <- (data_object$data %>% select(id) %>% distinct())[[1]]
+    wd <- getwd()
+    system("mkdir oneATT")
+    setwd("oneATT")
+    for (subject in 1:length(IDs)){
+        sub_data <- data_object$data %>% 
+                    filter(id == IDs[subject]) %>%
+                    PM_data$new()
+        Pmetrics::NPrun(model_path, sub_data, intern = T, ...)
+    }
+    setwd(wd)
 }
