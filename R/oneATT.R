f70df7bf9e37a62b8106a839cac3fe2af3d67d9b
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-09-26 12:13:44 -0500

    Generating the DENXXX file

--- R/oneATT.R
+++ R/oneATT.R
@@ -15,6 +15,20 @@ oneATT<-function(model, data, engine, ...){
         res <- PM_load(subject)
         spp[[subject]] <- res$final$data$popPoints
     }
+    
+    txt<-""
+    for (index in 1:length(spp)){
+        fv <- function(val){paste0(substr(sprintf("%20.18f",val),1,20),paste(rep(" ",8),collapse=""))}
+        line <- spp[[index]]
+        txt<-paste(txt,fv(line$Ka),fv(line$Ke),fv(line$V),fv(line$Tlag1),"1        \n",collapse="")
+    }
+    den_file <- readLines("1/outputs/DEN0001")
+    den_file[4] <- den_file[4] %>% stringr::str_replace(" 1", as.character(length(IDs)))
+    n_param <- as.integer(den_file[5])
+    den_file <- den_file[-(15+2*n_param)]
+    den_file <- append(den_file,strsplit(txt,"\n")[[1]],14+2*n_param)
     setwd(wd)
-    spp
+    writeLines(den_file,"DEN001")
 }
+
+oneATT(modEx,dataEx)
